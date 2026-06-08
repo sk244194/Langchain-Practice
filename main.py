578e77d805378a3b0c5597cbbf6489cc5ad13de6
Shubham <64083206+sk244194@users.noreply.github.com>
2026-06-08 23:21:03 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Always create a virtual enviroment before working on a project to install certain version of libraries needed for that project
+# uv venv
+
+# for dependencies you could only use requirements.txt to install it directly using uv add -r require... or pip install -r require...
+
+# Using groq api and gemini api -> Free
+
+
+# So i have also forked the github(Langchain) from which i am studying so read that as well
+
